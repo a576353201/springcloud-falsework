885f0d6c00d7831dcc2fb44b5b36878ea6f7d2cf
F-Kapok <5219824@qq.com>
2020-10-29 13:43:27 +0800

    update

---
+++ springcloud-config/src/main/java/com/fans/config/ConfigApplication.java
@@ -0,0 +1,19 @@
+package com.fans.config;
+
+import com.baomidou.mybatisplus.autoconfigure.MybatisPlusAutoConfiguration;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication(scanBasePackages = {"com.fans"}, exclude = {DataSourceAutoConfiguration.class, MybatisPlusAutoConfiguration.class})
+@EnableEurekaClient
+@EnableConfigServer
+public class ConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class, args);
+    }
+
+}
